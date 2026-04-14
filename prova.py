d4713b1b8623f68a9f700e2c7c89136916ba8045
Edenilson Freitas <freitaseddie126@gmail.com>
2026-04-14 19:15:11 +0000

    estudando para a prova de algoritmos

---
+++ prova.py
@@ -0,0 +1,23 @@
+
+"""questao 01"""
+# numero = int(input("Digite o número:"))
+# if numero > 0:
+#     print("Numero positivo")
+# else:
+#     print("Numero Negativo")
+
+"""Questao 02"""
+# nome = "Edenilson"
+# idade = 48
+# sexo = "masculino"
+# altura = 1.70
+
+# print(f"Esses são os Dados pessoas do cliente: {nome}, {idade}, {sexo}, {altura}" )
+
+"""questao 03"""
+"""como calcular dois numero"""
+
+numero1 = int(input("Digite primeiro o numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+soma = numero1+numero2
+print("O resultado: ", soma)
